bef4b1f5142252871296e580de3305078292ee32
sudeshnashetty1919 <123196315+sudeshnashetty1919@users.noreply.github.com>
2025-12-23 23:23:08 +0530

    Update BaseTest.java

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,7 +4,8 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.Listeners;           // For @Listeners
+import listeners.ExtentReportListener;           // Your listener class
 import factory.DriverFactory;
 
 import org.openqa.selenium.WebDriver;
