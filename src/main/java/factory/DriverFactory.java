b39d3851815b13c3cd23b3109d5a725cbc1f176f
sudeshnashetty1919 <123196315+sudeshnashetty1919@users.noreply.github.com>
2025-12-23 21:09:43 +0530

    Update DriverFactory.java

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -26,6 +26,7 @@ public class DriverFactory {
         options.addArguments("--disable-extensions");
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--disable-backgrounding-occluded-windows");
